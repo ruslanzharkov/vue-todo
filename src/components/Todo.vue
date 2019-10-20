6d71aa59b33190e4e78bd464563e960366402bb4
ruslanzharkov <zharkov.ruslan.e@gmail.com>
2019-10-20 20:39:32 +0600

    Created todo component

---
+++ src/components/Todo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    {{todo.title}}
+    <button v-on:click="removeTodo" class="rm">Remove</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+  props: ['todo', 'index'],
+
+  methods: {
+    removeTodo() {
+      this.$emit('removeTodos');
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
